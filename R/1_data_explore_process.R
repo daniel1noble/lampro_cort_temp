eb20ff6ff6e09538271ffaa8747b0e876773a312
Daniel Noble <daniel.noble@anu.edu.au>
2022-05-10 08:28:02 +1000

    Adding explore script

---
+++ R/1_data_explore_process.R
@@ -0,0 +1,12 @@
+######################################################
+# Data Processing and Exploratory Analysis 
+# Author:
+# Date:
+######################################################
+
+###############################
+# Importing and organizing data
+################################
+
+# Read data
+data <- read.csv("./data/data_21_22.csv")
