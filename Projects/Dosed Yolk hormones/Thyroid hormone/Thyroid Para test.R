7b36628d059f77e6d63c4f59b57d2cb119d5099e
kris-wild <kristofferwild@icloud.com>
2024-08-23 17:20:03 +1000

    Cleaning up folder and code
    Uploading code from local folder

--- Projects/Dosed Yolk hormones/Thyroid hormone/Thyroid Para test.R
+++
@@ -1,9 +0,0 @@
-T4Para <- read.csv ("C:/Users/U1115575/Documents/thyroid_para_test.csv")
-
-View(T4Para)
-T4Para$Type <- as.factor(T4Para$Type)
-
-T4Para$LogT4 <- log10((T4Para$T4_ng.ml))
-Para_Test <- lm(LogT4 ~ DilutionFactor + Type, data = T4Para)
-anova(Para_Test)
-summary(Para_Test)
--- README.md
+++
@@ -1,2 +0,0 @@
-# lampro_cort_temp
-Interaction between maternal corticosterone and offspring temperature in Lampropholis delicata
